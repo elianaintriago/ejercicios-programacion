edd4fd029592d5035fdf5bd560fb5e169c16284f
Valentina Intriago <eliana.intriago@udla.udla.edu.ec>
2026-05-13 15:18:09 -0500

    tercer commit se agrego aux

--- ejercicio 2.c
+++ ejercicio 2.c
@@ -2,8 +2,10 @@
 
 int main (int argc, char *argv[]) {
  char vocales[]={'a','e','i','o','u'};
- char *ptr;
+ char *ptr, *aux;
  ptr = &vocales[0];
+ aux =&vocales[4];
+*aux = 'x';
  for (int i = 0; i < 5; i++)
  {
     printf("vocales[%d]%c",i, vocales[i]);
