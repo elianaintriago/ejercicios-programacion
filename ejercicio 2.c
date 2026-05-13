72c5454cebce2f4223b63a799a974c0ffe1687ff
Valentina Intriago <eliana.intriago@udla.udla.edu.ec>
2026-05-13 15:13:47 -0500

    segundo commit

--- ejercicio 2.c
+++ ejercicio 2.c
@@ -2,10 +2,13 @@
 
 int main (int argc, char *argv[]) {
  char vocales[]={'a','e','i','o','u'};
+ char *ptr;
+ ptr = &vocales[0];
  for (int i = 0; i < 5; i++)
  {
     printf("vocales[%d]%c",i, vocales[i]);
- }
+    printf("%c\n",*(ptr++));
+     }
  
     return 0;
 }
